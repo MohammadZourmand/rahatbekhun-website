712473b6eeba21fea29506b31f11b051c77d715b
Mohammad <spystar0003@gmail.com>
2023-06-08 14:05:37 +0430

    add 404 page

--- src/components/global/navbar/searchModal/searchTypesData.ts
+++ src/components/global/navbar/searchModal/searchTypesData.ts
@@ -9,41 +9,41 @@ export const SearchTypesData : searchTypesDataProps[] = [
     {
         id : 1,
         type : "همه مطالب",
-        img : "./images/home/searchTypes/all.png",
+        img : "/images/home/searchTypes/all.png",
     },
     {
         id : 2,
         type : "صفر تا صد",
-        img : "./images/home/searchTypes/100.png",
+        img : "/images/home/searchTypes/100.png",
     },
     {
         id : 3,
         type : "کاربرگ",
-        img : "./images/home/searchTypes/worksheet.png",
+        img : "/images/home/searchTypes/worksheet.png",
     },
     {
         id : 4,
         type : "ویدیو",
-        img : "./images/home/searchTypes/video.png",
+        img : "/images/home/searchTypes/video.png",
     },
     {
         id : 5,
         type : "بازی",
-        img : "./images/home/searchTypes/game.png",
+        img : "/images/home/searchTypes/game.png",
     },
     {
         id : 6,
         type : "آزمون",
-        img : "./images/home/searchTypes/exam.png",
+        img : "/images/home/searchTypes/exam.png",
     },
     {
         id : 7,
         type : "داستان",
-        img : "./images/home/searchTypes/storybook.png",
+        img : "/images/home/searchTypes/storybook.png",
     },
     {
         id : 8,
         type : "مقالات",
-        img : "./images/home/searchTypes/articles.png",
+        img : "/images/home/searchTypes/articles.png",
     },
 ] 
---
+++ src/pages/404/index.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+import Lottie from "lottie-react";
+
+import Navbar from "@/components/global/navbar";
+import Paragraph from "@/components/global/elements/paragraph";
+import Heading4 from "@/components/global/elements/headings/h4";
+
+import error404 from "@/assets/animation/lottieFiles/error-404.json";
+
+const UndefinedPage : NextPage = () => {
+
+    return (
+        <>
+            <Navbar isWhite={false} />
+            <div className="grid grid-cols-12 items-center justify-center lg:mt-8 md:mt-16 mt-8 lg:mx-36 sm:mx-12 mx-6">
+                <Lottie className="sm:col-span-6 col-span-12" animationData={error404} />
+                <div className="sm:col-span-6 col-span-12 flex flex-col sm:items-start items-center">
+                    <Heading4 text="صفحه مورد نظرت پیدا نشد !" />
+                    <Paragraph cls="font-medium mt-4 leading-loose sm:!text-right !text-center" text="شاید آدرس صفحه رو اشتباه وارد کردی یا شاید این صفحه قدیمی هست ! شایدم ما داریم روی این صفحه کار می کنیم ولی در هر صورتی این صفحه الان وجود نداره که باز بشه برات !" />
+                    <Link className="text-baby-9 font-semibold mt-4 block" href={"/"}> برگردیم به خانه </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UndefinedPage;
